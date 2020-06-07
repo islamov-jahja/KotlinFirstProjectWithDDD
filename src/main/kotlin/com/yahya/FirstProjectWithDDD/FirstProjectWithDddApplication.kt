163341b6bfc96079e18be640bfaa00a82a0eb531
islamov-jahja <yahyaislamov15@mail.ru>
2020-06-07 20:09:54 +0300

    старт проекта

---
+++ src/main/kotlin/com/yahya/FirstProjectWithDDD/FirstProjectWithDddApplication.kt
@@ -0,0 +1,11 @@
+package com.yahya.FirstProjectWithDDD
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstProjectWithDddApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstProjectWithDddApplication>(*args)
+}
---
+++ src/test/kotlin/com/yahya/FirstProjectWithDDD/FirstProjectWithDddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yahya.FirstProjectWithDDD
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstProjectWithDddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
